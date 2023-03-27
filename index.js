be9b81f60ea1129d12b3ed6bd43f4417811d6941
Chandrashekhar gowda KS <106522596+Chandra583@users.noreply.github.com>
2023-03-27 05:51:53 +0000

    sc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let arr = [45,21,22] ;
-let a =arr.map((value, index)=>{
-  console.log(value,index);
+let a =arr.map((value, index, array)=>{
+  console.log(value,index,array);
   return value + 1 + index; 
 });
 console.log(a)
