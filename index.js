613fad18a2ef02b2e112e221d491897bf4f24280
Chandrashekhar gowda KS <106522596+Chandra583@users.noreply.github.com>
2023-03-28 17:06:17 +0000

    arra

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let arr = [45,21,22] ;
 let a =arr.map((value, index, array)=>{
   console.log(value,index,array);
-  return value + 1 + index; 
+  return value + 1 + index + array; 
 });
 console.log(a)
+console.log("HEllo world!")
